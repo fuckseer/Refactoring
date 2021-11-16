51eca1c93ed791d80b244cf7e1692ae42b029aca
fuckseer <ksusha.magic.96@gmail.com>
2021-11-17 00:07:30 +0500

    3 и 4 этап

--- filter.py
+++ filter.py
@@ -1,41 +1,38 @@
 from PIL import Image
 import numpy as np
-img = Image.open("img2.jpg")
+
+
+urlimage = input("Укажите имя файла")
+urlimage = urlimage+'.jpg'
+img = Image.open(urlimage)
 arr = np.array(img)
 length = len(arr)
-length1 = len(arr[1])
 size = 10
 gray = 6
 i = 0
 
+
 def get_sum_color():
     global n, n1, sum_color
     for n in range(i, i + size):
-        for n1 in range(j, j + size):
-            red = int(arr[n][n1][0])
-            green = int(arr[n][n1][1])
-            blue = int(arr[n][n1][2])
-            sum_color += red + green + blue
-    sum_color = int(sum_color // 100)
+        sum_color += np.sum(arr[n][j:j+size][:])
+    sum_color = sum_color // 100
 
 
 def get_gray_color():
     global n, n1
     for n in range(i, i + size):
-        for n1 in range(j, j + size):
-            arr[n][n1][0] = int(sum_color // 50) * 50 // gray
-            arr[n][n1][1] = int(sum_color // 50) * 50 // gray
-            arr[n][n1][2] = int(sum_color // 50) * 50 // gray
+        arr[n][j:j+size][:] = int(sum_color // 50) * 50 // gray
 
 
 while i < length - 1:
     j = 0
-    while j < length1 - 1:
+    while j < length - 1:
         sum_color = 0
         get_sum_color()
         get_gray_color()
         j = j + size
     i = i + size
+
 res = Image.fromarray(arr)
-res.show()
 res.save('res.jpg')
