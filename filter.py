ad821c38c9cef52c8b5686fc7273b1532897f81e
fuckseer <ksusha.magic.96@gmail.com>
2021-11-16 21:35:01 +0500

    2 этап

--- filter.py
+++ filter.py
@@ -2,27 +2,40 @@ from PIL import Image
 import numpy as np
 img = Image.open("img2.jpg")
 arr = np.array(img)
-a = len(arr)
-a1 = len(arr[1])
+length = len(arr)
+length1 = len(arr[1])
+size = 10
+gray = 6
 i = 0
-while i < a - 1:
+
+def get_sum_color():
+    global n, n1, sum_color
+    for n in range(i, i + size):
+        for n1 in range(j, j + size):
+            red = int(arr[n][n1][0])
+            green = int(arr[n][n1][1])
+            blue = int(arr[n][n1][2])
+            sum_color += red + green + blue
+    sum_color = int(sum_color // 100)
+
+
+def get_gray_color():
+    global n, n1
+    for n in range(i, i + size):
+        for n1 in range(j, j + size):
+            arr[n][n1][0] = int(sum_color // 50) * 50 // gray
+            arr[n][n1][1] = int(sum_color // 50) * 50 // gray
+            arr[n][n1][2] = int(sum_color // 50) * 50 // gray
+
+
+while i < length - 1:
     j = 0
-    while j < a1 - 1:
-        s = 0
-        for n in range(i, i + 10):
-            for n1 in range(j, j + 10):
-                n1 = int(arr[n][n1][0])
-                n2 = int(arr[n][n1][1])
-                n3 = int(arr[n][n1][2])
-                s += n1 + n2 + n3
-        s = int(s // 100)
-        for n in range(i, i + 10):
-            for n1 in range(j, j + 10):
-                arr[n][n1][0] = int(s//3)
-                arr[n][n1][1] = int(s//3)
-                arr[n][n1][2] = int(s//3)
-        j = j + 10
-    i = i + 10
+    while j < length1 - 1:
+        sum_color = 0
+        get_sum_color()
+        get_gray_color()
+        j = j + size
+    i = i + size
 res = Image.fromarray(arr)
 res.show()
 res.save('res.jpg')
