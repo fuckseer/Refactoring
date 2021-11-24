513732f66fa7179bb1b4a1ba5d92aa03892c5b28
fuckseer <ksusha.magic.96@gmail.com>
2021-11-24 15:17:25 +0500

    PEP8

--- filter.py
+++ filter.py
@@ -3,7 +3,7 @@ import numpy as np
 import doctest
 
 
-def convert_image_to_mosaic ( image , size , gradation_step ) :
+def convert_image_to_mosaic(image,size,gradation_step):
     """
 
         Convert image to mosaic
@@ -25,14 +25,14 @@ def convert_image_to_mosaic ( image , size , gradation_step ) :
             [ 45.,  45.,  45.]]])
 
     """
-    for x in range ( 0 , len ( image ) , size ) :
-        for y in range ( 0 , len ( image [ 0 ] ) , size ) :
-            image [ x :x + size , y :y + size ] = get_average_brightness (
-                image [ x :x + size , y :y + size ] , size , gradation_step )
+    for x in range(0, len(image), size) :
+        for y in range(0, len(image[0]), size):
+            image[x:x + size, y:y + size] = get_average_brightness(
+                image[x:x + size, y:y + size], size, gradation_step )
     return image
 
 
-def get_average_brightness ( block , size , gradation_step ) :
+def get_average_brightness(block, size, gradation_step):
     """
     Get average brightness of image
     param block: mosaic block size
@@ -52,22 +52,21 @@ def get_average_brightness ( block , size , gradation_step ) :
     96
     """
 
-    average_color = (block [ :size , :size ].sum () / 3) // size ** 2
-    res = int ( average_color // gradation_step ) * gradation_step
+    average_color = (block[:size, :size].sum() / 3) // size ** 2
+    res = int(average_color // gradation_step) * gradation_step
     return res
 
 
 def main () :
-    image_file = Image.open ( input ( "Введите имя файла, которое хотите конвертировать: " ) )
-    block_size = int ( input ( "Введите размер блока: " ) )
-    gradations_count = int ( input ( "Введите количество градаций серого: " ) )
-    image = np.array ( image_file )
+    image_file = Image.open(input("Введите имя файла, которое хотите конвертировать: "))
+    block_size = int(input("Введите размер блока: "))
+    gradations_count = int(input("Введите количество градаций серого: "))
+    image = np.array(image_file)
     gradation_step = 255 // gradations_count
 
-    res = Image.fromarray ( convert_image_to_mosaic ( image , block_size , gradation_step ) )
-    res.save ( input ( "Введите имя файла, в которой хотите сохранить результат: " ) )
+    res = Image.fromarray(convert_image_to_mosaic(image, block_size, gradation_step))
+    res.save(input("Введите имя файла, в которой хотите сохранить результат: "))
 
 
 if __name__ == '__main__' :
-    doctest.testmod ()
-    main ()
+    main()
